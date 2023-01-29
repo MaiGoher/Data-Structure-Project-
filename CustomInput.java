5103b3d743b5cf49f8118ab3ede16d5917302287
MaiGoher <91436067+MaiGoher@users.noreply.github.com>
2023-01-29 20:00:20 +0200

    Compression task

--- CustomInput.java
+++ CustomInput.java
@@ -1,9 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.dsproject;
 
+
+package project;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.util.NoSuchElementException;
